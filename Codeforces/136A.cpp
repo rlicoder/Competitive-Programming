3ee04435e1133a7c4fe8b7737d31eb9f5e41e0a4
rlicoder <rl.nguyen@outlook.com>
2020-12-27 16:08:57 -0800

    136A

---
+++ Codeforces/136A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << find(a.begin(), a.end(), i+1) - a.begin() + 1 << " ";
+    }
+}
+
+
