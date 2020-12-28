bf51f5beb7ebba0a9c9dc40632679f4d74e3914d
rlicoder <rl.nguyen@outlook.com>
2020-12-28 00:31:03 -0800

    1399B

---
+++ Codeforces/1399B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a,b(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+
+
