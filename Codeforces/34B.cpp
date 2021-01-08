28b5e118765c01a825611ab21253090b5c11b64b
rlicoder <rl.nguyen@outlook.com>
2021-01-08 14:45:39 -0800

    34B

---
+++ Codeforces/34B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int ans = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (a[i] < 0)
+        {
+            ans += abs(a[i]);
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << ans;
+}
