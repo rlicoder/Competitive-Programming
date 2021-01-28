01ca9b8a96e026664612f2d5c626e606f60005ec
rlicoder <rl.nguyen@outlook.com>
2021-01-27 19:39:14 -0800

    wesley and exam

---
+++ Code-E/wesley_and_exam.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        cout << a[0] << " " << a.back() << endl;
+    }
+}
